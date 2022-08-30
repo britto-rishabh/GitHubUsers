b940ee9d60816444c27b3c11d61345559c7c1c19
britto-rishabh <nilesh.pansuriya@rishabhsoft.com>
2022-08-30 20:42:57 +0530

    Reachability, Start on Coredata

---
+++ GitHubUsers/PersistenceManager.swift
@@ -0,0 +1,17 @@
+//
+//  PersistanceManager.swift
+//  GitHubUsers
+//
+//  Created by Britto Thomas on 30/08/22.
+//
+
+import Foundation
+
+enum PersistenceManager {
+    
+    static private let defaults = UserDefaults.standard
+    
+    static func update(note:String) -> Void {
+        
+    }
+}
