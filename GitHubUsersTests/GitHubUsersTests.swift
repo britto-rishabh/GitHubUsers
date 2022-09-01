212df730c8abdd2d78bdd2a4fdc1f6c3b6f7de97
britto-rishabh <nilesh.pansuriya@rishabhsoft.com>
2022-09-01 14:38:59 +0530

    Test Case

--- GitHubUsersTests/GitHubUsersTests.swift
+++ GitHubUsersTests/GitHubUsersTests.swift
@@ -9,28 +9,55 @@ import XCTest
 @testable import GitHubUsers
 
 class GitHubUsersTests: XCTestCase {
-
+    
     override func setUpWithError() throws {
-
+        
     }
-
+    
     override func tearDownWithError() throws {
-
+        
     }
-
+    
     func testGetUserList() throws {
+        let getUserList = GetUserList()
+        let parameter = GetUserListParameter(id: 0)
+        var users:[User]?
+        let usersExpectation = expectation(description: "users")
         
+        getUserList.call(parameter: parameter) {_users in
+            users = _users;
+            usersExpectation.fulfill()
+        }
+        
+        waitForExpectations(timeout: 1) { (error) in
+            XCTAssertNotNil(users)
+        }
     }
-
+    
     func testGetUserProfile() throws {
+        let getUserProfile = GetUserProfile()
+        let parameter = GetUserProfileParameter(name: "mojombo")
+        var userProfile:UserProfile?
+        let userProfileExpectation = expectation(description: "userProfile")
         
+        getUserProfile.call(parameter: parameter) {_userProfile in
+            userProfile = _userProfile;
+            userProfileExpectation.fulfill()
+        }
+        
+        waitForExpectations(timeout: 1) { (error) in
+            XCTAssertNotNil(userProfile)
+        }
     }
     
     func testGetUserNote() throws {
-        
+        let note = PersistenceManager.note(for: 1)
+        XCTAssertNotNil(note)
     }
     
     func testUpdateUserNote() throws {
-        
+        PersistenceManager.update(id: 2, note: "Hello")
+        let note = PersistenceManager.note(for: 2)
+        XCTAssertEqual(note, "Hello")
     }
 }
